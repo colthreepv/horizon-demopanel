42eadae6b0620c64f7fd1fbcb9defa82cd79f247
colthreepv <mister.gamer@gmail.com>
2018-04-10 13:22:09 +0200

    rename file etc

--- demopanel/enabled/_31000_demoplugin.py
+++ demopanel/enabled/_31000_demoplugin.py
@@ -1,17 +1,17 @@
 # The name of the panel to be added to HORIZON_CONFIG. Required.
-PANEL = 'mypanel'
+PANEL = 'demopanel'
 
 # The name of the dashboard the PANEL associated with. Required.
 PANEL_DASHBOARD = 'identity'
 
 # Python panel class of the PANEL to be added.
-ADD_PANEL = 'myplugin.content.mypanel.panel.MyPanel'
+ADD_PANEL = 'demoplugin.content.demopanel.panel.MyPanel'
 
 # A list of applications to be prepended to INSTALLED_APPS
-ADD_INSTALLED_APPS = ['myplugin']
+ADD_INSTALLED_APPS = ['demoplugin']
 
 # A list of AngularJS modules to be loaded when Angular bootstraps.
-ADD_ANGULAR_MODULES = ['horizon.dashboard.identity.myplugin.mypanel']
+# ADD_ANGULAR_MODULES = ['horizon.dashboard.identity.demoplugin.demopanel']
 
 # Automatically discover static resources in installed apps
 AUTO_DISCOVER_STATIC_FILES = True
@@ -20,7 +20,7 @@ AUTO_DISCOVER_STATIC_FILES = True
 ADD_JS_FILES = []
 
 # A list of scss files to be included in the compressed set of files
-ADD_SCSS_FILES = ['dashboard/identity/myplugin/mypanel/mypanel.scss']
+# ADD_SCSS_FILES = ['dashboard/identity/myplugin/mypanel/mypanel.scss']
 
 # A list of template-based views to be added to the header
-ADD_HEADER_SECTIONS = ['myplugin.content.mypanel.views.HeaderView',]
+ADD_HEADER_SECTIONS = ['demoplugin.content.demopanel.views.HeaderView',]
