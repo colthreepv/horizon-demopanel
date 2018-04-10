01063735693813afb980f03df8c525cc52a819aa
colthreepv <mister.gamer@gmail.com>
2018-04-10 21:02:52 +0200

    better install script

--- install-cats.sh
+++ install-cats.sh
@@ -3,6 +3,7 @@ OPENSTACK_DIR=${OPENSTACK_DIR:-/opt/stack}
 
 set -x
 
+sudo pip uninstall catsplugin -y
 sudo python setup.py install
 cd catsplugin
 cp -rv enabled "$OPENSTACK_DIR/horizon/openstack_dashboard/local/"
