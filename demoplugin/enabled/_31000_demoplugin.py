c2ca3892cbec397d48936a2c767a4516488bde2a
colthreepv <mister.gamer@gmail.com>
2018-04-10 14:10:19 +0200

    wtf

--- demoplugin/enabled/_31000_demoplugin.py
+++ demoplugin/enabled/_31000_demoplugin.py
@@ -1,11 +1,11 @@
 # The name of the panel to be added to HORIZON_CONFIG. Required.
-PANEL = 'demopanel'
+PANEL = 'demoplugin'
 
 # The name of the dashboard the PANEL associated with. Required.
 PANEL_DASHBOARD = 'identity'
 
 # Python panel class of the PANEL to be added.
-ADD_PANEL = 'demopanel.content.demopanel.panel.DemoPanel'
+ADD_PANEL = 'demoplugin.content.demopanel.panel.DemoPanel'
 
 # A list of applications to be prepended to INSTALLED_APPS
 ADD_INSTALLED_APPS = ['demoplugin']
@@ -23,4 +23,4 @@ ADD_JS_FILES = []
 # ADD_SCSS_FILES = ['dashboard/identity/myplugin/mypanel/mypanel.scss']
 
 # A list of template-based views to be added to the header
-ADD_HEADER_SECTIONS = ['demopanel.content.demopanel.views.IndexView',]
+ADD_HEADER_SECTIONS = ['demoplugin.content.demopanel.views.IndexView',]
