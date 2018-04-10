a713dfdf1065e1a4815172e8ed04a03237bc5dd4
colthreepv <mister.gamer@gmail.com>
2018-04-10 20:33:01 +0200

    (installscript): sudo missing

--- install-cats.sh
+++ install-cats.sh
@@ -3,6 +3,6 @@ OPENSTACK_DIR=${OPENSTACK_DIR:-/opt/stack}
 
 set -x
 
-python setup.py install
+sudo python setup.py install
 cd catsplugin
 cp -rv enabled "$OPENSTACK_DIR/horizon/openstack_dashboard/local/"
